ca44271c074ac2abdb36c3e6682e77f233f90a1f
Conless <conlesspan@outlook.com>
2024-02-01 19:13:29 +0800

    feat(interface): add support for threadIdx.

--- rtl/interface/gelato_init_if.sv
+++ rtl/interface/gelato_init_if.sv
@@ -5,16 +5,41 @@
 
 `include "gelato_types.svh"
 
-interface gelato_init_if;
+interface gelato_init_sm_if;
   import gelato_types::*;
 
   logic valid;
   addr_t pc;
+  int3_t gridDim;
+  int3_t blockDim;
+  int3_t blockIdx;
+
+  // GPU -> SM Controller
+  modport master(output valid, output pc, output gridDim, output blockDim, output blockIdx);
+
+  // SM Controller -> GPU
+  modport slave(input valid, input pc, input gridDim, input blockDim, input blockIdx);
+endinterface
+
+interface gelato_init_warp_if;
+  import gelato_types::*;
+
+  logic valid;
+  addr_t pc;
+  int3_t gridDim;
+  int3_t blockDim;
+  int3_t blockIdx;
   integer workers;
 
-  // GPU -> split table
-  modport master(output valid, output pc, output workers);
+  // SM Controller -> Split Table
+  modport master_split_table(output valid, output pc, output workers);
 
-  // split table -> GPU
-  modport slave(input valid, input pc, input workers);
+  // Split Table -> SM Controller
+  modport slave_split_table(input valid, input pc, input workers);
+
+  // SM Controller -> RF Arbiter
+  modport master_rf_arbiter(output valid, output pc, output gridDim, output blockDim, output blockIdx);
+
+  // RF Arbiter -> SM Controller
+  modport slave_rf_arbiter(input valid, input pc, input gridDim, input blockDim, input blockIdx);
 endinterface
