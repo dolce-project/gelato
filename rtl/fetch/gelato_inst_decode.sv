116e1bffd736e455a517f8e8cbc0499c771ae2ab
Conless <conlesspan@outlook.com>
2024-03-10 15:00:42 +0800

    refactor(decode): use three-stage state machine to rewrite idecode.

--- rtl/fetch/gelato_inst_decode.sv
+++ rtl/fetch/gelato_inst_decode.sv
@@ -15,130 +15,178 @@ module gelato_inst_decode (
   input logic rst_n,
   input logic rdy,
 
-  gelato_ifetch_idecode_if.slave inst_raw_data,  // From I-Fetch
-  gelato_idecode_split_if.master split_data,  // To split table, get thread mask and update table
-  gelato_idecode_ibuffer_if.master inst_decoded_data  // To I-Buffer
+  // Update split data
+  gelato_idecode_split_if.master split_data,
+
+  // Get the instruction raw data from ifetch
+  input logic din_valid,
+  output logic din_ready,
+  input inst_raw_data_t din,
+
+  // Send the decoded instruction to ibuffer
+  output logic dout_valid,
+  input logic dout_ready,
+  output inst_decoded_data_t dout
 );
 
+  //============================================================================
+  // Transition of state
+  //============================================================================
   typedef enum {
     IDLE,
+    DECODE,
     UPDATE
-  } status_t;
+  } state_t;
 
-  inst_t   decoded_inst;  // Decoded instruction
-  status_t status;
+  state_t state_q, state_d;
+  inst_raw_data_t inst_raw_data_q, inst_raw_data_d;  // Raw instruction
+  inst_t decoded_inst_q, decoded_inst_d;  // Decoded instruction
 
+  always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      state_q <= IDLE;
+    end else begin
+      state_q <= state_d;
+      inst_raw_data_q <= inst_raw_data_d;
+      decoded_inst_q <= decoded_inst_d;
+    end
+  end
 
-  function inst_t decode(addr_t pc, warp_num_t warp_num, data_t raw_inst);
-    inst_t inst;
-
-    inst.pc = pc;
-    inst.warp_num = warp_num;
-    inst.opcode = raw_inst[6:0];
-
-    case (inst.opcode)
-      `OPCODE_LUI, `OPCODE_AUIPC: begin
-        inst.rd  = raw_inst[11:7];
-        inst.imm = {raw_inst[31:12], 12'b0};
+  //============================================================================
+  // Condition of state transition
+  //============================================================================
+  always_comb begin
+    case (state_q)
+      IDLE: begin
+        if (inst_raw_data.valid) begin
+          state_d = DECODE;
+          inst_raw_data_d = inst_raw_data;
+        end
       end
-      `OPCODE_JAL: begin
-        inst.rd = raw_inst[11:7];
-        inst.imm = {
-          {12{raw_inst[31]}},
-          raw_inst[19:12],
-          raw_inst[20],
-          raw_inst[30:21],
-          1'b0
-        };
+      DECODE: begin
+        decoded_inst_d.pc = inst_raw_data_q.pc;
+        decoded_inst_d.warp_num = inst_raw_data_q.warp_num;
+        decoded_inst_d.opcode = inst_raw_data_q.inst_raw_data[6:0];
+        case (inst.opcode)
+          `OPCODE_LUI, `OPCODE_AUIPC: begin
+            decoded_inst_d.rd  = inst_raw_data_q.inst_raw_data[11:7];
+            decoded_inst_d.imm = {inst_raw_data_q.inst_raw_data[31:12], 12'b0};
+          end
+          `OPCODE_JAL: begin
+            decoded_inst_d.rd = inst_raw_data_q.inst_raw_data[11:7];
+            decoded_inst_d.imm = {
+              {12{inst_raw_data_q.inst_raw_data[31]}},
+              inst_raw_data_q.inst_raw_data[19:12],
+              inst_raw_data_q.inst_raw_data[20],
+              inst_raw_data_q.inst_raw_data[30:21],
+              1'b0
+            };
+          end
+          `OPCODE_JALR: begin
+            decoded_inst_d.rd = inst_raw_data_q.inst_raw_data[11:7];
+            decoded_inst_d.rs1 = inst_raw_data_q.inst_raw_data[19:15];
+            decoded_inst_d.imm = {
+              {20{inst_raw_data_q.inst_raw_data[31]}},
+              inst_raw_data_q.inst_raw_data[31:20]
+            };
+          end
+          `OPCODE_BRANCH: begin
+            decoded_inst_d.rs1 = inst_raw_data_q.inst_raw_data[19:15];
+            decoded_inst_d.rs2 = inst_raw_data_q.inst_raw_data[24:20];
+            decoded_inst_d.imm = {
+              {20{inst_raw_data_q.inst_raw_data[31]}},
+              inst_raw_data_q.inst_raw_data[7],
+              inst_raw_data_q.inst_raw_data[30:25],
+              inst_raw_data_q.inst_raw_data[11:8],
+              1'b0
+            };
+          end
+          `OPCODE_LOAD: begin
+            decoded_inst_d.rd = inst_raw_data_q.inst_raw_data[11:7];
+            decoded_inst_d.rs1 = inst_raw_data_q.inst_raw_data[19:15];
+            decoded_inst_d.imm = {
+              {20{inst_raw_data_q.inst_raw_data[31]}},
+              inst_raw_data_q.inst_raw_data[31:20]
+            };
+          end
+          `OPCODE_STORE: begin
+            decoded_inst_d.rs1 = inst_raw_data_q.inst_raw_data[19:15];
+            decoded_inst_d.rs2 = inst_raw_data_q.inst_raw_data[24:20];
+            decoded_inst_d.imm = {
+              {20{inst_raw_data_q.inst_raw_data[31]}},
+              inst_raw_data_q.inst_raw_data[31:25],
+              inst_raw_data_q.inst_raw_data[11:7]
+            };
+          end
+          `OPCODE_ARITHI: begin
+            decoded_inst_d.rd = inst_raw_data_q.inst_raw_data[11:7];
+            decoded_inst_d.rs1 = inst_raw_data_q.inst_raw_data[19:15];
+            decoded_inst_d.imm = {
+              {20{inst_raw_data_q.inst_raw_data[31]}},
+              inst_raw_data_q.inst_raw_data[31:20]
+            };
+          end
+          `OPCODE_ARITH: begin
+            decoded_inst_d.rd  = inst_raw_data_q.inst_raw_data[11:7];
+            decoded_inst_d.rs1 = inst_raw_data_q.inst_raw_data[19:15];
+            decoded_inst_d.rs2 = inst_raw_data_q.inst_raw_data[24:20];
+          end
+          `OPCODE_NOP: begin
+          end
+          default: begin
+            $fatal(0, "gelato_inst_decode: Invalid opcode");
+          end
+        endcase
+        state_d = UPDATE;
       end
-      `OPCODE_JALR: begin
-        inst.rd  = raw_inst[11:7];
-        inst.rs1 = raw_inst[19:15];
-        inst.imm = {{20{raw_inst[31]}}, raw_inst[31:20]};
-      end
-      `OPCODE_BRANCH: begin
-        inst.rs1 = raw_inst[19:15];
-        inst.rs2 = raw_inst[24:20];
-        inst.imm = {
-          {20{raw_inst[31]}}, raw_inst[7], raw_inst[30:25], raw_inst[11:8], 1'b0
-        };
-      end
-      `OPCODE_LOAD: begin
-        inst.rd  = raw_inst[11:7];
-        inst.rs1 = raw_inst[19:15];
-        inst.imm = {{20{raw_inst[31]}}, raw_inst[31:20]};
-      end
-      `OPCODE_STORE: begin
-        inst.rs1 = raw_inst[19:15];
-        inst.rs2 = raw_inst[24:20];
-        inst.imm = {{20{raw_inst[31]}}, raw_inst[31:25], raw_inst[11:7]};
-      end
-      `OPCODE_ARITHI: begin
-        inst.rd  = raw_inst[11:7];
-        inst.rs1 = raw_inst[19:15];
-        inst.imm = {{20{raw_inst[31]}}, raw_inst[31:20]};
-      end
-      `OPCODE_ARITH: begin
-        inst.rd  = raw_inst[11:7];
-        inst.rs1 = raw_inst[19:15];
-        inst.rs2 = raw_inst[24:20];
-      end
-      `OPCODE_NOP: begin
+      UPDATE: begin
+        state_d = IDLE;
       end
       default: begin
-        $fatal(0, "gelato_inst_decode: Invalid opcode");
+        $fatal(0, "gelato_inst_decode: Invalid state");
       end
     endcase
-  endfunction
+  end
 
-  always_ff @(posedge clk, negedge rst_n) begin
+  //============================================================================
+  // Output
+  //============================================================================
+  always_comb begin
     if (!rst_n) begin
-      status <= IDLE;
-      inst_decoded_data.valid <= 0;
-      inst_decoded_data.inst <= 0;
+      dout_valid = 0;
+      dout = 0;
     end else if (rdy) begin
-      case (status)
+      case (state_q)
         IDLE: begin
-          if (inst_raw_data.valid) begin
-            inst_raw_data.valid <= 0;
-            decoded_inst <= decode(
-              inst_raw_data.pc, inst_raw_data.warp_num, inst_raw_data.inst
-            );
-            split_data.warp_num <= inst_raw_data.warp_num;
-            split_data.split_table_num <= inst_raw_data.split_table_num;
-            status <= UPDATE;
-          end
-          split_data.valid <= 0;
-          inst_decoded_data.valid <= 0;
+          dout_valid = 0;
+          din_ready  = 1;
+        end
+        DECODE: begin
+          din_ready = 0;
         end
         UPDATE: begin
           // Deliver the decoded instruction to the I-Buffer
-          inst_decoded_data.inst  <= decoded_inst;
-
-          inst_decoded_data.valid <= 1;
-
+          dout_valid = 1;
+          dout = decoded_inst_q;
           $display(
             "Decode %h: opcode = %b, rd = %d, rs1 = %d, rs2 = %d, imm = %d",
-            decoded_inst.pc, decoded_inst.opcode, decoded_inst.rd,
-            decoded_inst.rs1, decoded_inst.rs2, decoded_inst.imm);
+            decoded_inst_q.pc, decoded_inst_q.opcode, decoded_inst_q.rd,
+            decoded_inst_q.rs1, decoded_inst_q.rs2, decoded_inst_q.imm);
 
           // Update the split table
-          split_data.valid <= 1;
-          split_data.activate <=
-            !(decoded_inst.opcode == `OPCODE_BRANCH &&
-            (decoded_inst.funct3 == `FUNCT3_BEQD || decoded_inst.funct3 == `FUNCT3_BNED));
-          if (decoded_inst.opcode == `OPCODE_AUIPC || decoded_inst.opcode == `OPCODE_BRANCH) begin
-            split_data.stall <= 1;
-          end else if (decoded_inst.opcode == `OPCODE_JAL || decoded_inst.opcode == `OPCODE_JALR) begin
-            split_data.stall <= 0;
-            split_data.updated_pc <= inst_raw_data.pc + decoded_inst.imm;
+          split_data.valid = 1;
+          split_data.activate =
+            !(decoded_inst_q.opcode == `OPCODE_BRANCH &&
+            (decoded_inst_q.funct3 == `FUNCT3_BEQD || decoded_inst_q.funct3 == `FUNCT3_BNED));
+          if (decoded_inst_q.opcode == `OPCODE_AUIPC || decoded_inst_q.opcode == `OPCODE_BRANCH) begin
+            split_data.stall = 1;
+          end else if (decoded_inst_q.opcode == `OPCODE_JAL || decoded_inst_q.opcode == `OPCODE_JALR) begin
+            split_data.stall = 0;
+            split_data.updated_pc = decoded_inst_q.pc + decoded_inst_q.imm;
           end else begin
-            split_data.stall <= 0;
-            split_data.updated_pc <= inst_raw_data.pc + 4;
+            split_data.stall = 0;
+            split_data.updated_pc = decoded_inst_q.pc + 4;
           end
-
-          // Update status
-          status <= IDLE;
         end
         default: begin
           $fatal(0, "gelato_inst_decode: Invalid status");
--- rtl/fetch/gelato_inst_fetch.sv
+++ rtl/fetch/gelato_inst_fetch.sv
@@ -38,11 +38,20 @@ module gelato_inst_fetch (
   } state_t;
   state_t state_q, state_d;
 
+  addr_t pc_q, pc_d;
+  warp_num_t warp_num_q, warp_num_d;
+  split_table_num_t split_table_num_q, split_table_num_d;
+  thread_mask_t thread_mask_q, thread_mask_d;
+
   always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
       state_q <= IDLE;
     end else if (rdy) begin
       state_q <= state_d;
+      pc_q <= pc_d;
+      warp_num_q <= warp_num_d;
+      split_table_num_q <= split_table_num_d;
+      thread_mask_q <= thread_mask_d;
     end
   end
 
@@ -52,7 +61,12 @@ module gelato_inst_fetch (
   always_comb begin
     case (state_q)
       IDLE: begin
-        state_d = din_valid ? WAIT_MEM : IDLE;
+        if (din_valid) begin
+          state_d = WAIT_MEM;
+          pc_d = din.pc;
+          warp_num_d = din.warp_num;
+          split_table_num_d = din.split_table_num;
+        end
       end
       WAIT_MEM: begin
         state_d = icache_if.ready ? WAIT_READY : WAIT_MEM;
@@ -69,27 +83,27 @@ module gelato_inst_fetch (
   //============================================================================
   // Output
   //============================================================================
-  always_ff @(posedge clk or negedge rst_n) begin
+  always_comb begin
     if (!rst_n) begin
-      dout_valid <= 0;
+      dout_valid = 0;
     end else if (rdy) begin
-      case (state_d)
+      case (state_q)
         IDLE: begin
-          dout_valid <= 0;
-          dout_ready <= 1;
-          dout.pc <= din.pc;
-          dout.warp_num <= din.warp_num;
-          dout.split_table_num <= din.split_table_num;
+          dout_valid = 0;
+          din_ready = 1;
         end
         WAIT_MEM: begin
-          dout_ready <= 0;
-          icache_if.valid <= 1;
-          icache_if.addr  <= din.pc;
+          din_ready = 0;
+          icache_if.valid = 1;
+          icache_if.addr  = pc_d;
         end
         WAIT_READY: begin
-          icache_if.valid <= 0;
-          dout_valid <= 1;
-          dout.inst_raw_data <= icache_if.data;
+          icache_if.valid = 0;
+          dout_valid = 1;
+          dout.pc = pc_q;
+          dout.warp_num = warp_num_q;
+          dout.split_table_num = split_table_num_q;
+          dout.inst_raw_data = icache_if.data;
         end
         default: begin
           $fatal(0, "gelato_inst_fetch: Invalid state");
